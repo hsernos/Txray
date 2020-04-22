815e03623943ea412578d36e65d006b1a27b1877
hsernos <hsernos@163.com>
2020-04-22 11:40:16 +0800

    添加启动服务时的状态检查

--- config/ctl.go
+++ config/ctl.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"bufio"
 	"os/exec"
+	"time"
 	log "v3ray/logger"
 	"v3ray/tool"
 )
@@ -22,12 +24,25 @@ func (c *Config) Start(i int) {
 	c.SaveJSON()
 	c.GenConfig()
 	if c.exeCmd == nil {
-		c.exeCmd = exec.Command("v2ray", "--config", tool.Join(c.getConfigPath() , "config.json"))
+		c.exeCmd = exec.Command("v2ray", "--config", tool.Join(c.getConfigPath(), "config.json"))
 	}
+	stdout, _ := c.exeCmd.StdoutPipe()
 	c.exeCmd.Start()
-	log.Info("v2ray --config ", tool.Join(c.getConfigPath() , "config.json"))
-	log.Info("开启v2ray服务, 监听端口：",c.Settings.Port,"，选定节点索引为：",c.Index)
-	
+	r := bufio.NewReader(stdout)
+	lines := new([]string)
+	go readInfo(r, lines)
+	code := new(int)
+	go checkProc(c.exeCmd, code)
+	time.Sleep(time.Duration(1) * time.Second)
+
+	if *code > 0 {
+		log.Error("开启v2ray服务失败,查看下面报错信息来检查出错问题")
+		for _, x := range *lines {
+			log.Error(x)
+		}
+	} else {
+		log.Info("开启v2ray服务成功, 监听端口：", c.Settings.Port, "，选定节点索引：", c.Index)
+	}
 }
 
 // Stop 停止v2ray服务
@@ -36,4 +51,19 @@ func (c *Config) Stop() {
 		c.exeCmd.Process.Kill()
 		c.exeCmd = nil
 	}
+}
+
+func readInfo(r *bufio.Reader, lines *[]string) {
+	for i := 0; i < 20; i++ {
+		line, _, _ := r.ReadLine()
+		if len(string(line[:])) != 0 {
+			*lines = append(*lines, string(line[:]))
+		}
+	}
+}
+
+// 检查进程状态
+func checkProc(c *exec.Cmd, code *int) {
+	c.Wait()
+	*code = c.ProcessState.ExitCode()
 }
